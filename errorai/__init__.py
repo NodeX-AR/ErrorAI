ad4b7a738afd4e503b5709e51ae402f946f0efd2
ASWANTH R <aswanthr82@gmail.com>
2026-07-26 20:32:21 +0530

    Update __init__.py

--- errorai/__init__.py
+++ errorai/__init__.py
@@ -1,7 +1,7 @@
 from .compat import catch_errors, global_activate, watch
 from .runtime import configure, get_runtime
 
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 __all__ = ["get_runtime", "configure", "watch", "catch_errors", "global_activate"]
 
 # Auto-start runtime on import
